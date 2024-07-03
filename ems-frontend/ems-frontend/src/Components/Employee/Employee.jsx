209668824798acc138676198a4bec7ffccd99174
Alabadilaa <alabadila.malai1@gmail.com>
2024-07-03 23:47:58 +0100

    Now validating the form before submitting it

--- ems-frontend/ems-frontend/src/Components/Employee/Employee.jsx
+++ ems-frontend/ems-frontend/src/Components/Employee/Employee.jsx
@@ -8,18 +8,56 @@ const Employee = () => {
     const [ lastName, setLastName] = useState('')
     const [ email, setEmail] = useState('')
 
+    const[ errors, setErrors ]  = useState({
+        firstName: '',
+        lastName: '',
+        email: ''
+    })
+
     const navigator = useNavigate();
 
     function saveEmployee(e) {
         e.preventDefault();
 
-        const employee = {firstName, lastName, email}
-        console.log(employee)
+        if(validateForm()) {
+            const employee = {firstName, lastName, email}
+            console.log(employee)
 
-        createEmployee(employee).then((response) => {
-            console.log(response.data);
-            navigator('/employees')
-        })
+            createEmployee(employee).then((response) => {
+                console.log(response.data);
+                navigator('/employees')
+            })
+        }
+
+        
+    }
+
+    function validateForm() {
+        let valid = true;
+        
+        const errorsCopy = {... errors}
+
+        if (firstName.trim()) errorsCopy.firstName = '';
+        else {
+            errorsCopy.firstName = 'First name is required';
+            valid = false;
+        }
+
+        if (lastName.trim()) errorsCopy.lastName = '';
+        else {
+            errorsCopy.lastName = 'Last name is required';
+            valid = false;
+        }
+
+        if (email.trim()) errorsCopy.email = '';
+        else {
+            errorsCopy.email = 'Email is required';
+            valid = false;
+        }
+
+        setErrors(errorsCopy);
+
+        return valid;
     }
 
   return (
@@ -37,9 +75,10 @@ const Employee = () => {
                                 placeholder='Enter Employee First Name' 
                                 name='firstName' 
                                 value={firstName}
-                                className='form-control'
+                                className= {`form-control ${errors.firstName ? 'is-invalid': ''}`}
                                 onChange={(e) => setFirstName(e.target.value)}>
                             </input>
+                            {errors.firstName && <div className='invalid-feedback'> {errors.firstName} </div>}
                         </div>
 
                         <div className='form-group mb-2'>
@@ -49,9 +88,10 @@ const Employee = () => {
                                 placeholder='Enter Employee Last Name' 
                                 name='lastName' 
                                 value={lastName}
-                                className='form-control'
+                                className= {`form-control ${errors.lastName ? 'is-invalid': ''}`}
                                 onChange={(e) => setLastName(e.target.value)}>
                             </input>
+                            {errors.lastName && <div className='invalid-feedback'> {errors.lastName} </div>}
                         </div>
 
                         <div className='form-group mb-2'>
@@ -61,9 +101,10 @@ const Employee = () => {
                                 placeholder='Enter Employee Email' 
                                 name='email' 
                                 value={email}
-                                className='form-control'
+                                className= {`form-control ${errors.email ? 'is-invalid': ''}`}
                                 onChange={(e) => setEmail(e.target.value)}>
                             </input>
+                            {errors.email && <div className='invalid-feedback'> {errors.email} </div>}
                         </div>
 
                         <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
